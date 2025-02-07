336e096e92b547809f5d71c6d480c25ff9432be7
0x777A <wangzhen199125@gmail.com>
2025-02-07 12:15:10 +0800

    add cpmm initialize random pool example

--- cpmm-cpi/programs/cpmm-cpi/src/instructions/mod.rs
+++ cpmm-cpi/programs/cpmm-cpi/src/instructions/mod.rs
@@ -1,11 +1,13 @@
 pub mod proxy_deposit;
 pub mod proxy_initialize;
+pub mod  proxy_initialize_random_pool;
 pub mod proxy_swap_base_input;
 pub mod proxy_swap_base_output;
 pub mod proxy_withdraw;
 
 pub use proxy_deposit::*;
 pub use proxy_initialize::*;
+pub use proxy_initialize_random_pool::*;
 pub use proxy_swap_base_input::*;
 pub use proxy_swap_base_output::*;
 pub use proxy_withdraw::*;
